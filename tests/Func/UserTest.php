c751a6678bff6e2a798dc7ef112955d4ee68a4b8
elhathoutWobz <135958250+elhathoutWobz@users.noreply.github.com>
2023-06-17 15:08:54 -0700

    2 éme test funct StoreOneUser

--- tests/Func/UserTest.php
+++ tests/Func/UserTest.php
@@ -3,9 +3,12 @@ namespace  App\Tests\Func;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use Faker\Factory;
 class UserTest extends  AbstractEndPoint
 {
+    private string $userPayload = '{"email":"%s","password":"password"}';
+
+    //get all users
      public  function testGetUsers(): void
     {
         //get response
@@ -20,5 +23,37 @@ class UserTest extends  AbstractEndPoint
         self::assertJson($responseContent);
         //tester que respone est non vide
         self::assertNotEmpty($responseDecoded);
+    }
+    //create one user
+    public  function testPostUser():void
+    {
+
+        //get response
+        $response=$this->GetResponseFromRequest(
+            Request::METHOD_POST,
+            'http://127.0.0.1:8000/api/users',
+            $this->getPayload()
+
+        );
+
+        //get content
+        $responseContent = $response->getContent();
+
+        //decode content
+        $responseDecoded = json_decode($responseContent);
+
+        //test status code
+        self::assertEquals(Response::HTTP_CREATED,$response->getStatusCode());
+        //test if content are format json
+        self::assertJson($responseContent);
+        //tester que respone est non vide
+        self::assertNotEmpty($responseDecoded);
+    }
+
+    //function to get fake email
+    private function getPayload() :string
+    {
+    $faker = Factory::create();
+    return sprintf($this->userPayload,$faker->email);
     }
 }
