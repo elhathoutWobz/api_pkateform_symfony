f460300850e1b29a70800f43579e8365e335715a
elhathoutWobz <135958250+elhathoutWobz@users.noreply.github.com>
2023-06-16 21:19:27 -0700

    complet test unit

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Unit;
 
+use App\Entity\Article;
 use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
@@ -33,4 +34,43 @@ class UserTest extends TestCase
         // Assert that the getUserIdentifier method returns the same email
         self::assertEquals($email, $this->user->getUserIdentifier());
     }
+
+    public  function testGetRoles() :void
+    {
+        $role = ['ROLE_ADMIN'];
+
+    $res = $this->user->setRoles($role);
+        self::assertInstanceOf(User::class,$res);
+        self::assertContains('ROLE_USER',$this->user->getRoles());
+        self::assertContains('ROLE_ADMIN',$this->user->getRoles());
+    }
+
+    public  function testGetPassword() :void
+    {
+        $pass = 'password';
+
+        $res = $this->user->setPassword($pass);
+        self::assertInstanceOf(User::class,$res);
+        self::assertEquals($pass, $this->user->getPassword());
+
+
+    }
+
+    public function testGetArticle() :void
+    {
+        $article = new Article();
+
+        $res= $this->user->addArticle($article);
+        self::assertInstanceOf(User::class,$res);
+        self::assertCount(1,$this->user->getArticles());
+        self::assertTrue($this->user->getArticles()->contains($article));
+
+        $res=$this->user->removeArticle($article);
+        self::assertInstanceOf(User::class,$res);
+        self::assertCount(0,$this->user->getArticles());
+        self::assertFalse($this->user->getArticles()->contains($article));
+
+    }
+
+
 }
