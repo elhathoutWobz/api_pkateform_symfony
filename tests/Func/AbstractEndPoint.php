ed7db634cb426f840d087574d17dae061d5488d1
elhathoutWobz <135958250+elhathoutWobz@users.noreply.github.com>
2023-06-17 14:39:10 -0700

    1 ére test funct getUsers

--- tests/Func/AbstractEndPoint.php
+++ tests/Func/AbstractEndPoint.php
@@ -1,24 +1,26 @@
 <?php
-
+namespace App\Tests\Func;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 abstract class AbstractEndPoint extends  WebTestCase
 {
     private array $serverInformation=['ACCEPT'=>'application/json','CONTENT_TYPE'=>'application/json'];
-    public  function testGetResponseFromRequest(string $method,string $url,string $payload): Response
+    public  function GetResponseFromRequest(string $method,string $url,string $payload=''): Response
     {
-
+        //create client
         $client= self::createClient();
-        $client->request(
-            $method,
-            $url.'.json',
-        [],
-            $this->serverInformation,
-            $payload
-        );
+        //make request
+            $client->request(
+                $method,
+                $url.'.json',
+                [],
+                [],
+                $this->serverInformation,
+                $payload
+            );
 
-
+        //return response
        return $client->getResponse();
     }
 }
--- tests/Func/UserTest.php
+++ tests/Func/UserTest.php
@@ -1,18 +1,24 @@
 <?php
 namespace  App\Tests\Func;
 
-
-use JetBrains\PhpStorm\NoReturn;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class UserTest extends  WebTestCase
+class UserTest extends  AbstractEndPoint
 {
      public  function testGetUsers(): void
     {
-
-        $client= self::createClient();
-        $client->request(Request::METHOD_GET,'/api/users.json');
-       dd($client->getResponse());
+        //get response
+        $response=$this->GetResponseFromRequest(Request::METHOD_GET,'http://127.0.0.1:8000/api/users');
+       //get content
+        $responseContent = $response->getContent();
+        //decode content
+        $responseDecoded = json_decode($responseContent);
+       //test status code
+        self::assertEquals(Response::HTTP_OK,$response->getStatusCode());
+        //test if content are format json
+        self::assertJson($responseContent);
+        //tester que respone est non vide
+        self::assertNotEmpty($responseDecoded);
     }
 }
