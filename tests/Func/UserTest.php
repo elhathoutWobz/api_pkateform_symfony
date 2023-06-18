d3a8f30b0e694ec903495258e444b502397c5cce
elhathoutWobz <135958250+elhathoutWobz@users.noreply.github.com>
2023-06-17 20:23:23 -0700

    3éme test delete and update user

--- tests/Func/UserTest.php
+++ tests/Func/UserTest.php
@@ -7,7 +7,12 @@ use Faker\Factory;
 class UserTest extends  AbstractEndPoint
 {
     private string $userPayload = '{"email":"%s","password":"password"}';
-
+    //function to get fake email
+    private function getPayload() :string
+    {
+        $faker = Factory::create();
+        return sprintf($this->userPayload,$faker->email);
+    }
     //get all users
      public  function testGetUsers(): void
     {
@@ -49,11 +54,57 @@ class UserTest extends  AbstractEndPoint
         //tester que respone est non vide
         self::assertNotEmpty($responseDecoded);
     }
+    //get one user
 
-    //function to get fake email
-    private function getPayload() :string
+    public  function testGetUser(): void
     {
-    $faker = Factory::create();
-    return sprintf($this->userPayload,$faker->email);
+        //get response
+        $response=$this->GetResponseFromRequest(Request::METHOD_GET,'http://127.0.0.1:8000/api/users/1');
+        //get content
+        $responseContent = $response->getContent();
+        //decode content
+        $responseDecoded = json_decode($responseContent);
+        //test status code
+        self::assertEquals(Response::HTTP_OK,$response->getStatusCode());
+        //test if content are format json
+        self::assertJson($responseContent);
+        //tester que respone est non vide
+        self::assertNotEmpty($responseDecoded);
+    }
+
+    public  function testDeleteUser(): void
+    {
+        //get response
+        $response=$this->GetResponseFromRequest(Request::METHOD_DELETE,'http://127.0.0.1:8000/api/users/3');
+        //get content
+        $responseContent = $response->getContent();
+        //decode content
+        $responseDecoded = json_decode($responseContent);
+        //test status code
+        self::assertEquals(Response::HTTP_NO_CONTENT,$response->getStatusCode());
+
+    }
+
+    //update user
+    public  function testUpdateUser(): void
+    {
+        //get response
+        //get response
+        $response=$this->GetResponseFromRequest(
+            Request::METHOD_PUT,
+            'http://127.0.0.1:8000/api/users/4',
+            $this->getPayload()
+
+        );
+        //get content
+        $responseContent = $response->getContent();
+        //decode content
+        $responseDecoded = json_decode($responseContent);
+        //test status code
+        self::assertEquals(Response::HTTP_OK,$response->getStatusCode());
+        //test if content are format json
+        self::assertJson($responseContent);
+        //tester que respone est non vide
+        self::assertNotEmpty($responseDecoded);
     }
 }
